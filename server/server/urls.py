9f60b1ca560e0a73b47208a1d4a32980f6afd46a
王康 <1612890@mail.nankai.edu.cn>
2018-08-14 12:13:16 +0800

    User login-create user table!

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
-  path('', views.index),
-  path('api/', include([
-    path('random/', views.random)
-  ])),
+    path('', views.index),
+    path('api/', include([path('random/', views.random)])),
+    path('admin', admin.site.urls)
 ]
