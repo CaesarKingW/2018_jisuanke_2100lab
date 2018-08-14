c719eb99f36fb973a7b8f35b8796964eaa3ac591
王康 <1612890@mail.nankai.edu.cn>
2018-08-14 18:30:56 +0800

    Design and create manager_table and operating_history history!

--- server/project_lab/admin.py
+++ server/project_lab/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Manager, Operating_history
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Manager)
+admin.site.register(Operating_history)
--- server/server/urls.py
+++ server/server/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('api/', include([path('random/', views.random)])),
-    path('admin', admin.site.urls)
+    path('admin/', admin.site.urls)
 ]
