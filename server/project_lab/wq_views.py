e475803a4049a4ba575c68cb814eda9f796c9c69
111 <1612891@mail.nankai.edu.cn>
2018-08-15 09:51:30 +0800

    Upper solved. Ref#22

--- server/project_lab/wq_views.py
+++ server/project_lab/wq_views.py
@@ -1,5 +1,5 @@
 from .models import User
 def search_user(request):
     phone_number = request.POST.get('phone_number')
-    user = User.Objects.get(phone_number = phone_number)
+    user = User.objects.get(phone_number = phone_number)
     return render(request, 'user_managing.html', {'user':user})
