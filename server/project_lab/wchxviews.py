2d915f4b5c8ad3359609e8dd7e6f1bc3f9d61647
vladimir <1612889@mail.nankai.edu.cn>
2018-08-15 11:26:28 +0800

    Add Manage Search. Ref #4

--- server/project_lab/wchxviews.py
+++ server/project_lab/wchxviews.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, redirect
 def course_title(request):
   title = request.POST.get('title')
   course = Course.objects.filter(title=title)
-  return render(request,'course_edit.html')
+  return render(request,'course_edit.html', {'course',course})
 def Manager_name(request):
   username = request.POST.get('username')
-  Manager = Manager.objects.filter(username = username)
-  return render(request,'Manage_limits.html')
+  manager = Manager.objects.filter(username = username)
+  return render(request,'Manage_limits.html', {'manager', manager})
