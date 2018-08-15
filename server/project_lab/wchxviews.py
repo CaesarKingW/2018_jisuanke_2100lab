3ad2fac95e34e9c586700ba60eb7308e345ddd0c
vladimir <1612889@mail.nankai.edu.cn>
2018-08-15 11:03:08 +0800

    Add manage search. Ref #4

--- server/project_lab/wchxviews.py
+++ server/project_lab/wchxviews.py
@@ -4,3 +4,7 @@ def course_title(request):
   title = request.POST.get('title')
   course = Course.objects.filter(title=title)
   return render(request,'course_edit.html')
+def Manager_name(request):
+  username = request.POST.get('username')
+  Manager = Manager.objects.filter(username = username)
+  return render(request,'Manage_limits.html')
