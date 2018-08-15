f8897a6c15f6013afbbfa5660cc7cce8ef83a929
111 <1612891@mail.nankai.edu.cn>
2018-08-15 10:05:07 +0800

    Authenticate the user(write view). Ref#27

--- server/project_lab/wq_views.py
+++ server/project_lab/wq_views.py
@@ -3,3 +3,7 @@ def search_user(request):
     phone_number = request.POST.get('phone_number')
     user = User.objects.get(phone_number = phone_number)
     return render(request, 'user_managing.html', {'user':user})
+def anthenticate(request):
+    phone_number = request.POST.get('anthenticate')
+    User.objects.get(phone_number = phone_number).Is_teacher = True
+    return redirect('user_managing')
