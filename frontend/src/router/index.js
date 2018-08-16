134a99b949f62206c2ca197440d411feceb1b378
thevolga <635742300@qq.com>
2018-08-16 14:50:24 +0800

    Add a free content column.Ref #23

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import UserLogin from '@/components/UserLogin'
+import home from '@/components/home'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/UserLogin',
       name: 'UserLogin',
       component: UserLogin
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
     }
   ]
 })
