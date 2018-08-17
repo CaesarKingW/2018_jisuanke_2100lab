58de4be45161895c829bce587924b5570939fdfd
wangkang <1612890@mail.nankai.edu.cn>
2018-08-18 04:41:07 +0800

    Add backend unit test. Ref #8

--- server/project_lab/tests.py
+++ server/project_lab/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.test import Client
+from .utils.yunpian import YunPian
+
 
 # Create your tests here.
+#验证get请求
+class GetTest(TestCase):
+    def test_show_message(self):
+        response = self.client.get('/app/show_message')
+        self.assertEqual(response.status_code, 200)
+
+
+#验证post请求
+class PostTest(TestCase):
+    def test_add_message(self):
+        response = self.client.post('/app/add_message')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_code_post(self):
+        response = self.client.post('/app/get_code_post')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_new_user(self):
+        response = self.client.post('/app/register_new_user')
+        self.assertEqual(response.status_code, 200)
+
+
+#验证短信发送功能
+class SendMessage(TestCase):
+    def test_yunpian(self):
+        yun_pian = YunPian("264fb31e3ba88e5c55572dd977b2f372")
+        yun_pian.send_sms("6666", "13102250021")
+        self.assertEqual(yun_pian.send_state, '发送成功')
