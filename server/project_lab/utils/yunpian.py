1fadf67b2db8c6204c2eaa404a229916533b95a8
王康 <1612890@mail.nankai.edu.cn>
2018-08-18 04:51:56 +0800

    Add backend unit test!

--- server/project_lab/utils/yunpian.py
+++ server/project_lab/utils/yunpian.py
@@ -4,6 +4,9 @@ import json
 
 
 class YunPian(object):
+    send_state = ""
+
+
     def __init__(self, api_key):
         self.api_key = api_key
         self.single_send_url = "https://sms.yunpian.com/v2/sms/single_send.json"
@@ -17,4 +20,9 @@ class YunPian(object):
 
         response = requests.post(self.single_send_url, data=parmas)
         re_dict = json.loads(response.text)
+        self.send_state = re_dict['msg']
         print(re_dict)
+    
+# if __name__=="__main__":
+#       yun_pian = YunPian("264fb31e3ba88e5c55572dd977b2f372")
+#       yun_pian.send_sms("2222", "13102250021")
