ab48fbd14cf0d00fdea41c50c945aa02303ba44b
thevolga <635742300@qq.com>
2018-08-19 19:55:59 +0800

    Change some small problems in grammar.Ref #46

--- frontend/src/components/ShowUserInfo.vue
+++ frontend/src/components/ShowUserInfo.vue
@@ -5,24 +5,6 @@
     <div id="phone_div">手机号码：{{ phone }}</div>
     <div id="award_div">奖励金数：<Icon type="logo-usd" />{{ award }}</div>
     <Button style="margin: 10px;" size="large" type="primary">修改个人信息</Button>
-    <div id="course">
-    <div style="background:#eee;padding: 20px">
-        <Card :bordered="false">
-            <p slot="title">已学课程</p>
-            <ul style="list-style:none;">
-                <li>实验室制取CO2</li>
-            </ul>
-        </Card>
-    </div>
-    <div style="background:#eee; padding: 20px">
-        <Card :bordered="false">
-            <p slot="title">已购课程</p>
-            <ul style="list-style:none;">
-                <li>自由落体运动</li>
-            </ul>
-        </Card>
-    </div>
-    </div>
 </div>
 </template>
 <script>
