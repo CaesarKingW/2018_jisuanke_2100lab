203dddd4913d6c8b2816fd50c9981d19411b4e4a
Alice-Megatron <2407748337@qq.com>
2018-08-20 11:22:28 +0800

    Fix bug. Ref #17

---
+++ frontend/src/components/back/deny.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="sorry">
+    <nav>
+      对不起，您无权访问此页面
+    </nav>
+    <div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'deny',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sorry {
+  padding: 200px 1px 200px 400px;
+  font-size: 40px;
+}
+</style>
--- frontend/src/components/backstage.vue
+++ frontend/src/components/backstage.vue
@@ -25,7 +25,7 @@ export default {
 }
 </script>
 
-<style scopeds>
+<style scoped>
 .fl {
   float: left;
 }
