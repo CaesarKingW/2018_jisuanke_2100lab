5a2620522c2d5c46d87fcbd83a4e53a534d39bfd
111 <1612891@mail.nankai.edu.cn>
2018-08-21 21:53:42 +0800

    Finish the function of user, comment and order and close the 30th issue. Ref #30

--- frontend/src/components/back/comment.vue
+++ frontend/src/components/back/comment.vue
@@ -1,16 +1,20 @@
 <template>
-  <section>
-    <div class="front">
-      <form>
-        用户id：
-        <input type="text" name="course_name" v-model="phone_number">
-        <button type="button" @click="search_user_comment">搜索</button>
-      </form>
+<div>
+    <div>
+        <input v-model="phone_number" placeholder='请输入用户手机号以搜索其留言'> <button @click="search()">搜索</button>
     </div>
-    <div class="front">
-      <Page :total="100" />
+    <div v-show='is_show'>
+    <div v-if = 'if_user===false'>
+        对不起，您搜索的用户不存在
     </div>
-  </section>
+    <div v-else-if='if_comment===false'>
+        对不起，不存在您所搜索的用户的留言
+    </div>
+    <div v-else>
+        <ul><li v-for="message in messages" :key="message.id">用户{{phone_number}}在{{message.created_at}}时对第{{message.course_id}}号课程《{{message.course_title}}》发表评论“{{message.content}}”。<button @click="delete_comment(message.id)">删除</button></li></ul>
+    </div>
+    </div>
+</div>
 </template>
 
 <script>
@@ -18,30 +22,68 @@ export default {
   name: 'comment',
   data() {
     return {
-      phone_number: ''
+      is_show: false,
+      phone_number: '',
+      if_user: null,
+      if_comment: null,
+      messages: []
     }
   },
   methods: {
-    search_user_comment() {
-      console.log(this.phone_number)
-      // eslint-disable-next-line
-      var phone_number = JSON.stringify(this.phone_number)
-      this.$http.post('http://192.168.55.33:8000/app/user_comment', phone_number)
+    search() {
+      var phoneNumber = JSON.stringify(this.phone_number)
+      this.$http
+        .post('http://192.168.55.33:8000/app/search_comment', phoneNumber)
         .then(response => {
-          console.log(response.data)
+          var res = response.data
+          console.log(res)
+          if (res.if_user === false) {
+            this.if_user = false
+          } else {
+            if (res.if_comment === false) {
+              this.if_comment = false
+            } else {
+              this.if_user = true
+              this.if_comment = true
+              this.messages = res.messages
+              console.log(this.messages)
+            }
+          }
+        })
+      this.is_show = true
+    },
+    delete_comment(messageId) {
+      alert('删除成功！')
+      console.log(this.phone_number)
+      console.log(messageId)
+      this.$http
+        .post(
+          'http://192.168.55.33:8000/app/delete_comment',
+          JSON.stringify({
+            message_id: messageId,
+            phone_number: this.phone_number
+          })
+        )
+        .then(response => {
+          var res = response.data
+          console.log(res)
+          if (res.if_user === false) {
+            this.if_user = false
+          } else {
+            if (res.if_comment === false) {
+              this.if_comment = false
+            } else {
+              this.if_user = true
+              this.if_comment = true
+              this.messages = res.messages
+              console.log(this.messages)
+            }
+          }
         })
     }
   }
 }
 </script>
 
-<style scoped>
-.front {
-  position: static;
-  padding-top: 50px;
-  padding-bottom: 400px;
-  padding-left: 400px;
-  font-size: 20px;
-}
-
+<style>
 </style>
--- server/project_lab/serializer.py
+++ server/project_lab/serializer.py
@@ -1,7 +1,42 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Message, Course, Order, Course_picture
+
 
 class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    fields = ('phone_number', 'user_name', 'head_protrait', 'welfare', 'Can_comment', 'Is_teacher')
+    class Meta:
+        model = User
+        fields = ('phone_number', 'user_name', 'head_protrait', 'welfare',
+                  'Can_comment', 'Is_teacher')
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    course_title = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Message
+        fields = ('id', 'user_phone', 'course_title', 'course_id', 'content',
+                  'created_at', 'praise_count')
+
+    def get_course_title(self, obj):
+        return obj.course_id.title
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ('Order_number', 'user_phone', 'course_id', 'amount_of_money',
+                  'status', 'create_at')
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('title', 'brief_introduction', 'audio', 'whole_introduction',
+                  'Is_distory', 'distory_time', 'Is_free', 'price',
+                  'share_rate', 'can_comment')
+
+
+class Course_pictureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course_picture
+        fields = ('course_id', 'course_picture', 'start_time', 'end_time')
