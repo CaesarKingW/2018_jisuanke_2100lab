7908b5dbfab446b3e5a48e13c016971ef220f2ab
thevolga <635742300@qq.com>
2018-08-22 11:52:02 +0800

    Add a foot in app.Ref #81

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
   <router-view />
+  <Layout>
+  <Footer><div id="foot">Copyright © 2018 2100实验室-佛组 All Rights Reserved.</div></Footer>
+  </Layout>
   </div>
 </template>
 <script>
@@ -8,11 +11,14 @@ export default {
   name: 'App'
 }
 </script>
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+#foot {
+  opacity: 1;
+  text-align: center;
 }
 </style>
