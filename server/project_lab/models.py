026e340b1b5c7d5f7ee0cae839cc879403bd7ec5
wangkang <1612890@mail.nankai.edu.cn>
2018-08-22 19:50:05 +0800

    Implement pseudo-delete and Refactoring table. Ref #96

--- server/project_lab/models.py
+++ server/project_lab/models.py
@@ -12,6 +12,7 @@ class User(models.Model):
     welfare = models.FloatField(default=0.0)
     Can_comment = models.BooleanField(default=True)
     Is_teacher = models.BooleanField(default=False)
+    exists = models.BooleanField(default=True)
 
     def __str__(self):
         return self.phone_number
@@ -96,6 +97,7 @@ class Message(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
     praise_count = models.IntegerField(default=0)
+    exists = models.BooleanField(default=True)
 
     def __str__(self):
         return str(self.id)
@@ -118,3 +120,12 @@ class Praise(models.Model):
     message_id = models.ForeignKey(
         "Message", verbose_name=("留言编号"), on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
+
+
+class Course_picture(models.Model):
+    id = models.AutoField(primary_key=True)
+    course_id = models.ForeignKey("Course", on_delete=models.CASCADE)
+    course_picture = models.ImageField(
+        ("课程图片"), upload_to='course_picture', blank=True, null=True)
+    start_time = models.DurationField((""))
+    end_time = models.DurationField((""))
