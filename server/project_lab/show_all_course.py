1ab19d9b82495a489580efcf59512f6d60595185
111 <1612891@mail.nankai.edu.cn>
2018-08-29 17:02:45 +0800

    Fix delete comment bugs.

--- server/project_lab/show_all_course.py
+++ server/project_lab/show_all_course.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, HttpResponse, redirect
 from project_lab.models import *
@@ -63,69 +62,3 @@ def get_specified_course(request):
         response['error_num'] = 1
 
     return JsonResponse(response)
-=======
-# -*- coding: utf-8 -*-
-from django.shortcuts import render, HttpResponse, redirect
-from project_lab.models import *
-from django.views.decorators.http import require_http_methods
-import json
-from django.core import serializers
-from django.http import JsonResponse
-
-
-@require_http_methods(['POST', 'GET'])
-def show_free_course(request):
-    response = {}
-    try:
-        if request.method == 'GET':
-            course = Course.objects.filter(price=0).order_by('-created_at')
-            response['list'] = json.loads(
-                serializers.serialize("json", course))
-            response['msg'] = 'success'
-            response['error_num'] = 0
-    except Exception as e:
-        response['msg'] = str(e)
-        response['error_num'] = 1
-
-    return JsonResponse(response)
-
-
-@require_http_methods(['POST', 'GET'])
-def show_paying_course(request):
-    response = {}
-    try:
-        if request.method == 'GET':
-            course = Course.objects.exclude(price=0).order_by('-created_at')
-            response['list'] = json.loads(
-                serializers.serialize("json", course))
-            response['msg'] = 'success'
-            response['error_num'] = 0
-    except Exception as e:
-        response['msg'] = str(e)
-        response['error_num'] = 1
-
-    return JsonResponse(response)
-
-
-@require_http_methods(['POST', 'GET'])
-def get_specified_course(request):
-    response = {}
-    try:
-        if request.method == 'POST':
-            req = json.loads(request.body.decode('utf-8'))
-            try:
-                temp = Course.objects.get(id=req)
-                course = Course.objects.filter(id=req)
-                response['list'] = json.loads(
-                    serializers.serialize("json", course))
-                response['exist'] = True
-            except:
-                response['exist'] = False
-            response['msg'] = 'success'
-            response['error_num'] = 0
-    except Exception as e:
-        response['msg'] = str(e)
-        response['error_num'] = 1
-
-    return JsonResponse(response)
->>>>>>> b3ca2c98027fb8bbb4a791f254a06d19d78be98a
