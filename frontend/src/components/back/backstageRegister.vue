74a052f8c6cb5543dd7c729520eccf39488d88b9
thevolga <635742300@qq.com>
2018-08-30 20:45:26 +0800

    Beautify the backstage UI.Ref #276

--- frontend/src/components/back/backstageRegister.vue
+++ frontend/src/components/back/backstageRegister.vue
@@ -1,46 +1,36 @@
 <template>
-  <div>
+  <div id="backstageRegister">
+    <div id="regColumn">
+      <div id="mobilePanel"></div>
+      <div id="title"><h1>2100实验室后台注册</h1></div>
     <Form ref="formInline" :model="formInline">
       <FormItem prop="user">
-        <i-input type="text" v-model="formInline.user" placeholder="Username" @on-blur="check()">
+        <div id="userInputDiv"><i-input type="text" id="userInput" v-model="formInline.user" placeholder="请输入用户名" @on-blur="check()">
           <Icon type="ios-person-outline" slot="prepend"></Icon>
         </i-input>
-        <div v-if="blank1">
-          <p v-if="if_blank1" class="blank">
-            用户名不能为空
-          </p>
-        </div>
-        <div v-if="prompt1">
-          <p v-if="if_exist" id="exist">用户名已存在</p>
-          <p v-else id="notexist">该用户名可使用</p>
         </div>
       </FormItem>
       <FormItem prop="password">
-        <i-input type="password" v-model="formInline.password" placeholder="Password" @on-blur="check_two()">
+        <div id="pwdOneDiv"><i-input type="password" id="pwdOneInput" v-model="formInline.password" placeholder="请输入密码" @on-blur="check_two()">
           <Icon type="ios-lock-outline" slot="prepend"></Icon>
         </i-input>
-        <div v-if="blank2">
-          <p v-if="if_blank2" class="blank">
-            密码不能为空
-          </p>
         </div>
       </FormItem>
       <FormItem prop="confirm">
-        <i-input type="password" v-model="formInline.confirm" placeholder="Password-comfirm" @on-blur="Confirm()">
+        <div id="pwdTwoDiv">
+        <i-input type="password" id="pwdTwoInput" v-model="formInline.confirm" placeholder="请确认密码" @on-blur="Confirm()">
           <Icon type="ios-eye-outline" slot="prepend" />
         </i-input>
-        <div v-if="prompt2">
-          <p v-if="if_match" id="matched">两次密码一致</p>
-          <p v-else id="unmatched">两次密码不一致</p>
         </div>
       </FormItem>
       <FormItem>
-        <Button type="primary" @click="register()">Register</Button>
+       <div id="regBtnDiv"><Button type="primary" id="regButton" @click="register()">立刻注册</Button></div>
       </FormItem>
       <FormItem>
-        <Button type="primary" @click="login()" v-if="can_login">使用新注册的帐号登录</Button>
+        <div id="logBtnDiv"><Button type="primary" id="logButton" @click="login()" v-if="can_login">立刻登录</Button></div>
       </FormItem>
     </Form>
+  </div>
   </div>
 </template>
 
@@ -69,6 +59,7 @@ export default {
       if (this.formInline.user === '') {
         this.if_blank1 = true
         this.blank1 = true
+        this.$Message.warning('用户名不可为空！')
       } else {
         this.if_blank1 = false
         this.blank1 = true
@@ -82,6 +73,11 @@ export default {
           .then(response => {
             var res = response.data
             this.if_exist = res['if_exist']
+            if (res['if_exist'] === true) {
+              this.$Message.error('用户名已存在！')
+            } else {
+              this.$Message.success('用户名可用！')
+            }
             this.prompt1 = true
           })
       }
@@ -89,6 +85,7 @@ export default {
     check_two() {
       if (this.formInline.password === '') {
         this.if_blank2 = true
+        this.$Message.warning('密码不可为空！')
       } else {
         this.if_blank2 = false
       }
@@ -97,8 +94,10 @@ export default {
     Confirm() {
       if (this.formInline.password === this.formInline.confirm) {
         this.if_match = true
+        this.$Message.success('两次输入密码一致！')
       } else {
         this.if_match = false
+        this.$Message.error('两次输入密码不一致！')
       }
       this.prompt2 = true
     },
@@ -118,7 +117,7 @@ export default {
             })
           )
           .then(response => {
-            alert('注册成功！')
+            this.$Message.success('注册成功！')
             this.can_login = true
           })
       }
@@ -133,22 +132,117 @@ export default {
           })
         )
         .then(response => {
-          alert('登录成功！')
+          this.$Message.success('登录成功！')
           this.$router.push({ name: 'backstage' })
         })
     }
   }
 }
 </script>
-<style>
-#matched,
-#notexist {
-  color: green;
+<style scoped>
+#regColumn {
+  width: 35%;
+  margin-top: 6%;
+  margin-left: 32%;
+  transition: opacity 1s;
+  -webkit-transition: opacity 1s;
+  background-size: 100% 100%;
+  border: #f1f1f1 solid 1px;
+  border-radius: 8px;
 }
-
-#unmatched,
-#exist,
-.blank {
-  color: red;
+#regColumn:hover {
+  box-shadow: 5px 5px 2px #888888;
+}
+#regColumn h1 {
+  background: #075182;
+  padding: 20px 0;
+  font-size: 160%;
+  font-weight: 15px;
+  text-align: center;
+  color: #fff;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+}
+#userInputDiv,
+#pwdOneDiv,
+#pwdTwoDiv {
+  margin-top: 5%;
+  width: 95%;
+  margin-left: 2.5%;
+  margin: 0 auto;
+}
+#userInputDiv {
+  margin-top: 5%;
+}
+#regButton,
+#logButton {
+  background-color: #fff;
+  color: #000;
+  font-size: 15px;
+  border: #000 solid 1px;
+  border-radius: 8px;
+  width: 90px;
+  height: 35px;
+  margin: 0 auto;
+  text-align: center;
+  position: static;
+}
+#regBtnDiv,
+#logBtnDiv {
+  margin-left: 40%;
+}
+@media screen and (max-width: 500px) {
+  #regColumn {
+    width: 95%;
+    margin-top: 6%;
+    margin-left: 2.5%;
+    transition: opacity 1s;
+    -webkit-transition: opacity 1s;
+    background-size: 100% 100%;
+    border: #f1f1f1 solid 1px;
+    border-radius: 8px;
+    margin-top: 120px;
+  }
+  #regColumn:hover {
+    box-shadow: 5px 5px 2px #888888;
+  }
+  #regColumn h1 {
+    background: #075182;
+    padding: 20px 0;
+    font-size: 160%;
+    font-weight: 15px;
+    text-align: center;
+    color: #fff;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+  }
+  #userInputDiv,
+  #pwdOneDiv,
+  #pwdTwoDiv {
+    margin-top: 5%;
+    width: 95%;
+    margin-left: 2.5%;
+    margin: 0 auto;
+  }
+  #userInputDiv {
+    margin-top: 5%;
+  }
+  #regButton,
+  #logButton {
+    background-color: #fff;
+    color: #000;
+    font-size: 15px;
+    border: #000 solid 1px;
+    border-radius: 8px;
+    width: 90px;
+    height: 35px;
+    margin: 0 auto;
+    text-align: center;
+    position: static;
+  }
+  #regBtnDiv,
+  #logBtnDiv {
+    margin-left: 38%;
+  }
 }
 </style>
